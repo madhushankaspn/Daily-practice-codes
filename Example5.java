2baa0a15dde12bbad128bb1d0841ae799b4c2777
Supun Madhusanka <tasmadhusanka@students.nsbm.ac.lk>
2025-12-17 13:11:21 +0530

    add Example5.java

---
+++ Example5.java
@@ -0,0 +1,22 @@
+class Customer{
+	int id;
+	int name;
+}
+class Items{
+	int code;
+	int qty;
+}
+class Example5{
+	public static void main(String [] args){
+		Customer c1;
+		Items i1;
+		
+		c1 = new Customer();
+		i1 = new Items();
+		
+		i1 = new Items();
+		//i1 = new Customer();
+		
+	}
+}	
+		
