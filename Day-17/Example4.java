6363a6e0896c909ba2579dfd75d318f783b282e0
Supun Madhusanka <tasmadhusanka@students.nsbm.ac.lk>
2025-12-25 21:35:29 +0530

    Instance block2

--- Day-17/Example4.java
+++ Day-17/Example4.java
@@ -1,4 +1,4 @@
-class A{
+/*class A{
 	int a;
 	
 	A(){
@@ -16,4 +16,56 @@ class Example4{
 		A a1=new A();
 		System.out.println("A: "+a1.a);
 	}
-}	
+}*/	
+
+class A{
+   int a;
+
+
+   A(){
+       a=100;
+   }
+   {
+       a=10;
+   }
+   A(int i){
+   }
+
+
+}
+
+
+/*
+* class A{
+   int a;
+
+
+   A(){
+       a=10;
+       a=100;
+   }
+   {
+
+
+   }
+   A(int i){
+   * a=10;
+   }
+
+
+}
+*
+*
+* */
+
+
+class Example{
+   public static void main(String[] args){
+       A a1=new A();
+       System.out.println("A:"+a1.a); //100
+
+
+       A a2=new A(1);
+       System.out.println("A:"+a2.a); //10
+   }
+}
