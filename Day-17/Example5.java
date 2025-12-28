995d5bc727f1df673bbc65921e589028d2241de7
Supun Madhusanka <tasmadhusanka@students.nsbm.ac.lk>
2025-12-28 12:29:59 +0530

    Add Example5

--- Day-17/Example5.java
+++ Day-17/Example5.java
@@ -1,4 +1,4 @@
-class Value{
+/*class Value{
     int a;
     Value(int a){this.a=a;}
 }
@@ -14,4 +14,32 @@ class Example5{
         ChangeValue.incrementValue(v1.a);
         System.out.println("Value is "+v1.a);//? --->  100
     }
+}*/
+
+
+//======Example 02===========
+
+class Value{
+   int a;
+   Value(int a){this.a=a;}
+}
+class ChangeValue{
+   public static void incrementValue(int a){
+       a++;
+   }
+
+
+   public static void incrementValueByObject(Value v){
+       v.a++;
+   }
+}
+class Example5{
+   public static void main(String[] args) {
+       Value v1 = new Value(100);
+       System.out.println("Value is "+v1.a);//100
+       ChangeValue.incrementValue(v1.a);
+       System.out.println("Value is "+v1.a);//100
+       ChangeValue.incrementValueByObject(v1);
+       System.out.println("Value is "+v1.a);//?
+   }
 }
