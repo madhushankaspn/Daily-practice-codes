37437a4a1df6f2ef80930d707aa8f3b867efa5f0
Supun Madhusanka <tasmadhusanka@students.nsbm.ac.lk>
2026-01-26 11:26:37 +0530

    Add Example5

--- Day-18/Example5.java
+++ Day-18/Example5.java
@@ -1,22 +1,25 @@
-class Example5 {
-
-    // instance method
-    public void instanceMethod() {
+class Example5{
+    public void instanceMethod () {
         System.out.println("instanceMethod");
     }
 
-    // static method
-    public static void staticMethod() {
+
+    public static void staticMethod () {
+
         System.out.println("staticMethod");
     }
 
     public static void main(String[] args) {
 
-        // calling static method (correct way)
-        Example5.staticMethod();
+        staticMethod();//ok
+        Example5.staticMethod();//ok
+        //instanceMethod(); //Error
+        //Demo.instanceMethod(); //Error
 
-        // creating object to call instance method
-        Example5 d1 = new Example5();
+        Example5 d1=new Example5();
         d1.instanceMethod();
+
+
+
     }
 }
